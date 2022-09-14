303a8bf4461a71b52bdbd93e448c1afcdb9c2d99
matty <matt@unixhead.org>
2022-09-14 21:21:34 +0100

    FEC versions

---
+++ FEC/rxFileTransferDiode.py
@@ -0,0 +1,204 @@
+
+
+#
+# THIS IS A POC ONLY AND SHOULD NOT BE USED IN PRODUCTION
+#
+
+
+#
+# For paid support/development activities, please visit https://otnss.co.uk
+#
+# For open source support, please visit https://github.com/unixhead/pyFileTransferDiode
+#
+
+# rxFileTransferDiode
+# Receives transfers via UDP from txFileTransferDiode.py and writes them to an output folder.
+# Duplicate files get a timestamp appended to the file name
+#
+# Original source: https://github.com/unixhead/pyFileTransferDiode
+
+# Beerware license
+
+
+from socket import *
+import os 
+from os.path import exists
+import random
+import struct
+import time
+import zfec
+
+# IP address to listen on, this should be filtered by the OS or network architecture so only the transmit side can reach it. 
+fileIP = "127.0.0.1"
+filePort = 10337
+
+#size of data to be transferred - TODO - make this an MTU setting
+chunkSize=1450
+fileNameSize=12 #size of filename field
+
+headerStr="III"+str(fileNameSize)+"sI"
+formatStr="III"+str(fileNameSize)+"sI"+str(chunkSize)+"s"
+packetSize=struct.calcsize(formatStr)
+headerSize = struct.calcsize(headerStr)
+
+#name of folder to store files in 
+#use the full path for this to avoid potential security issues, the filenames do get basic filtering but there's a degree of assumed trust
+outputFolder = "out"
+
+#TODO - check outfolder exists
+
+maxPacket = 1500
+
+#outFile="out/out" + str(random.randrange(10000000,90000000))
+#fOut = open(outFile, "wb")
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind((fileIP, filePort))
+
+def getTimeMS():
+    return int(time.time()*1000)
+
+def debugLog(data):
+    print(data)
+
+
+
+writing = False
+currentFile = False
+
+
+#
+# Main loop
+#
+while True:
+    message, address = serverSocket.recvfrom(packetSize)
+
+    #currentSerial,totalPackets,fileName,dataSize = struct.unpack(headerStr, message)
+    #debugLog("packet received " + str(len(message)) + " bytes")
+
+    # Packet format is:
+    # messageType (int) - 0 = data transfer, 1 = FEC transfer, 69 = checksum, 99 = error
+    # currentSerial (int)
+    # totalPackets (int)
+    # filename (char[fileNameSize])
+    # dataSize (int) - size of data in this packet
+    # bytes[chunkSize] - actual data
+
+    packetType, currentSerial,totalPackets,fileName, dataSize = struct.unpack_from(headerStr, message, 0)
+
+    if dataSize > maxPacket or dataSize < 0:
+        debugLog("error in datasize")
+        continue
+
+    # packetType 0 = data transfer
+    # type 99 = error code
+    if packetType == 99:        
+        dataStr = "<" + str(dataSize) + "s"
+        data = struct.unpack_from(dataStr, message, headerSize)[0]
+        debugLog("ERROR: " + str(data.decode("utf-8")))
+        continue # stop processing
+
+
+
+    fileName = str(fileName.decode("utf-8")).replace('\0','') # strip out null characters from filename (padded to 12 bytes)
+    #debugLog("pre-filtered + " + fileName)
+    fileName = fileName.replace('..','') #strip out potential parent directory command
+    fileName = fileName.replace('/','') # remove path
+    #debugLog("receiving + " + fileName)
+    #debugLog(str(currentSerial) + " / " + str(totalPackets) + " - file: " + fileName + " size: " + str(dataSize))
+
+    
+    # check if creating a new file
+    if writing == False:
+        if packetType == 1 or packetType == 2:
+            debugLog("receiving FEC")
+            # if it's a fec then write it to outputFolder + "/temp" + filename -fec/
+            tmpFecPath = outputFolder + "/" + fileName + "-fec"
+            if not os.path.isdir(tmpFecPath):
+                os.mkdir(tmpFecPath)
+
+            fecList = os.listdir(tmpFecPath)
+            outFile = tmpFecPath + "/fec" + str(len(fecList) + 1)
+            debugLog("writing: " + outFile)
+        elif packetType == 0: 
+            # if it's a regular file then write straight to the out folder
+            outFile = outputFolder + "/" + fileName
+            debugLog("receiving " + outFile)
+            # see if file already exists
+            if os.path.exists(outFile): # if it does then name this file with a timestamp afterwards
+                outFile = outFile + str(getTimeMS())
+
+        fOut = open(outFile, "wb")
+        writing = True
+        currentFile = fileName
+
+    # read the data bytes from the packet
+    dataStr = "<" + str(dataSize) + "s"
+    pktSize=struct.calcsize(dataStr)
+    data = struct.unpack_from(dataStr, message, headerSize)[0]
+    #debugLog("writing " + str(len(data)) + " bytes")
+    
+
+    # check the packet is part of the current file we're writing and not an overlapping transfer
+    # assuming all good then write the data
+    if currentFile != fileName:
+
+        #TODO - set a timer on this as it may mean the old transfer dropped packets, so need to force close it and start on the new one
+        
+        debugLog("packet received for incorrect file")
+        continue
+    else:
+        fOut.write(data)
+    
+    # If this is the final packet then close file handles
+    if (currentSerial == (totalPackets-1)):
+        #debugLog("Final packet")
+        fOut.close()
+        writing = False
+        currentFile = False
+
+        # if it's a FEC and we've received the final FEC chunk, then need to run zunfec
+        #TODO - turn this into a function so can call it from above too if we dropped packets
+        if packetType == 2:
+            # type 2 is the last FEC in a series
+            tmpFecPath = outputFolder + "/" + fileName + "-fec"
+            debugLog("reassembling " + fileName + " from fecs in " + tmpFecPath)
+            
+            #zunfec
+            
+            # get file to write
+            outFile = outputFolder + "/" + fileName
+            if os.path.exists(outFile): # if it does then name this file with a timestamp afterwards
+                outFile = outFile + str(getTimeMS())    
+            fOut = open(outFile, "wb")
+
+            # get list of handles to the FECs
+            tmpFecPath = outputFolder + "/" + fileName + "-fec"
+            fecs = os.listdir(tmpFecPath)
+
+            fecHandles = []
+            for fec in fecs:
+                fecName = tmpFecPath + "/" + fec    
+                fecHandles.append(open(fecName, 'rb'))
+
+            zfecWorked = True
+            try:
+                zfec.filefec.decode_from_files(fOut, fecHandles, verbose=False)
+            except:
+                zfecWorked = False
+
+
+            #check worked
+            if zfecWorked:
+                debugLog("Successfully wrote " + outFile)
+                #delete fecs
+                for fec in fecs:
+                    os.remove(tmpFecPath + "/" + fec)
+                
+                #delete tmpfecpath
+                os.rmdir(tmpFecPath)
+            else:
+                debugLog("unfec failed, leaving raw files in output directory " + str(tmpFecPath))
+
+
+print("done")
+
---
+++ FEC/txFileTransferDiode.py
@@ -0,0 +1,225 @@
+
+
+#
+# THIS IS A POC ONLY AND SHOULD NOT BE USED IN PRODUCTION
+#
+
+
+#
+# For paid support/development activities, please visit https://otnss.co.uk
+#
+# For open source support, please visit https://github.com/unixhead/pyFileTransferDiode
+#
+
+# txFileTransferDiode.py
+# Detects new files in a folder (inFolder) and transmits via UDP for sending over uni-directional links
+#
+# Original source: https://github.com/unixhead/pyFileTransferDiode
+
+# Beerware license
+
+
+import os 
+from socket import *
+import struct
+import time
+import zfec
+import math
+
+# how frequently to check folder for new files in seconds
+pollTime=5
+
+# Will transmit files received in the inFolder and move them to sentFolder
+# To clear out sentFolder, another process is needed.
+#
+#use the full path for this to avoid potential security issues, the filenames do get basic filtering but there's a degree of assumed trust
+inFolder="in"
+sentFolder="sent"
+
+tempFolder="temp"
+
+#where to send files
+sendToIP="127.0.0.1"
+sendToPort=10337
+fileNameSize=12 #size of filename field, you can increase this but it's put in every packet header so avoid going too large as it'll reduce throughput
+
+
+#FEC ratio - must be between 0 and 1
+#This is the redundancy requirement for the forward error correction, it sets how many blocks must successfully arrive to reassemble the file
+# e.g. 0.5 = 50% of blocks must arrive
+# 0.8 = 80% of blocks must arrive
+defaultFecRatio = 0.8
+if defaultFecRatio > 1 or defaultFecRatio < 0:
+    print("defaultFecRatio must be between 0 and 1, recommend set to 0.8")
+    exit
+
+#size of data to be transferred - TODO - make this an MTU setting
+chunkSize=1450 # size of data in each packet
+
+# introduced a delay between packets to lower chances of drops
+# if network is a bit slower then may need to increase this, but obviously decreases throughput
+packetDelay = 0.001
+#delay between sending files to give remote end a chance to unzfec and tidy up
+fileDelay = 0.1
+
+#Internal variable - packet format
+formatStr="III"+str(fileNameSize)+"sI"+str(chunkSize)+"s"
+
+
+def debugLog(data):
+    print(data)
+
+#return unix timestamp in milliseconds
+def getTimeMS():
+    return int(time.time()*1000)
+
+
+# Error signalling - send a fault message and quit afterwards
+def sendError(error):
+    error = error + "The transmitting program will now exit and need manually restarting once the error is resolved."
+    debugLog("Sending error packet: " + error)
+    s = socket(type=SOCK_DGRAM)
+    errMsg = bytes(error,"utf-8")
+    errCode = bytes("ERROR", "utf-8")
+    txData=struct.pack(formatStr, 99 , 1, 1,errCode, len(errMsg), errMsg)
+    s.sendto(txData,(sendToIP,int(sendToPort)))
+    s.close()
+    exit(0)
+
+
+#called with a filepath/name and transmits it to the remote site.
+def sendFile(fileName, type = 0, overrideFileName = False):
+    debugLog("sendFile " + str(fileName) + " type: " + str(type))
+    s = socket(type=SOCK_DGRAM)
+    fH = open(fileName, "rb")
+    fileSize=os.stat(fileName).st_size
+    debugLog("Sending file: " + str(fileName) + " - size: " + str(fileSize))
+    num = int( fileSize / chunkSize) + 1
+    i=0
+
+    # build packet and transmit
+    while i < num:
+        # Packet format is:
+        # messageType (int) - 0 = data transfer, 1 = FEC transfer, 2 = last FEC packet, 69 = checksum, 99 = error
+        # currentSerial (int)
+        # totalPackets (int)
+        # filename (char[fileNameSize])
+        # dataSize (int) - size of data in this packet
+        # bytes[chunkSize] - actual data 
+
+        dataChunk = fH.read(chunkSize)
+        
+        dataSize=len(dataChunk)
+        if type == 0:
+            fileNameTrimmed = fileName.replace(inFolder + "/", '') # remove the path for filename value that goes into packet header
+        elif type == 1 or type == 2:
+            fileNameTrimmed = overrideFileName.replace(inFolder + "/", '') # for FECS then use the original file name rather than the fec itself
+
+        #debugLog("trimmed name: " +fileNameTrimmed)
+        fileNameEncoded=bytes(fileNameTrimmed,"utf-8") # will get truncated to fileNameSize when packed, or null padded if shorter
+        
+        txData=struct.pack(formatStr, type , i, num,fileNameEncoded, dataSize, dataChunk)
+        s.sendto(txData,(sendToIP,int(sendToPort)))
+        i+=1
+        #delay 1ms to try and avoid dropped packets
+        time.sleep(packetDelay)
+
+    # Finished sending, close handles for file and socket
+    fH.close()
+    s.close()
+
+
+# uses zfec to create a bunch of FEC files, sends those over network.
+def processFile(fileName):
+    debugLog("Processing file " + fileName)
+    # work out K & M values
+    # minimum for K = 10 (how many blocks needed to recreate file)
+    # minimum for M = 20 (how many blocks)
+    
+    
+    numBlocks = 20
+    minGoodBlocks = int(defaultFecRatio * numBlocks)
+
+    fileSize=os.stat(fileName).st_size
+    # if file is larger than 1GB then probably want to use a few more chunks
+    if  fileSize > (100 * 1024 * 1024): # 100meg
+        # round up so we always end up with too many blocks rather than too few
+        debugLog("File over 1gig, sending in 100 blocks instead")
+        numBlocks = 100
+        minGoodBlocks = int(defaultFecRatio * numBlocks)
+    elif fileSize > (1000 * 1024 * 1024):
+        #bigger than 1 gig
+        numGigs = int(fileSize / (1000 * 1024 * 1024))
+        numBlocks = numGigs * 200
+        minGoodBlocks = int(defaultFecRatio * numBlocks)
+
+
+    fH = open(fileName, "rb")
+    zfec.filefec.encode_to_files(fH, fileSize, tempFolder, "tmp",  minGoodBlocks, numBlocks, ".fec", True, True)
+    fecList = os.listdir(tempFolder)
+    n=1
+    nFecs = len(fecList)
+    for fec in fecList:
+        # send the FEC, last one needs to be type 2
+        if n==nFecs:
+            packetType = 2
+        else: 
+            packetType = 1
+
+        #send it
+        sendFile(tempFolder + "/" + fec, packetType, fileName)
+
+        # delete the file
+        os.remove(tempFolder + "/" + fec)
+
+        n+=1
+
+        # wait between transmissions => attempt to reduce dropped packets
+        time.sleep(fileDelay)
+
+
+# Main loop, watches for files appearing in the "inFolder" and then sends them. 
+
+while True:
+    #debugLog("Main loop")
+
+    #check folders exist
+    if not os.path.exists(inFolder):
+        sendError("The folder : " + str(inFolder) + " does not exist, please create it or update configuration to the correct path for new files.")
+
+    if not os.path.exists(sentFolder):
+        sendError("The folder : " + str(sentFolder) + " does not exist, please create it or update configuration to the correct path for new files.")
+
+    if not os.path.exists(tempFolder):
+        sendError("The folder : " + str(tempFolder) + " does not exist, please create it or update configuration to the correct path for new files.")
+
+
+    # look for any new folders in the inFolder
+    fileList = os.listdir(inFolder)
+    for file in fileList:
+        if os.path.isfile(inFolder + "/" + file): # only send files in the directory, not links, other directories, etc
+            debugLog("Got new file: " + str(file))
+
+            #process it
+            processFile(inFolder + "/" + file)
+            
+
+            #TODO send checksum across
+
+
+            #now move it to the archive folder
+            outFile = sentFolder + "/" + file
+            if os.path.exists(outFile): # if it does then name this file with a timestamp afterwards
+                outFile = outFile + str(getTimeMS())
+
+            try:
+                os.rename(inFolder + "/" + file, outFile)
+            except PermissionError: 
+                sendError("Permission error writing to the output folder.")
+            except OSError as error:          
+                sendError("Failed to move file to output folder, error was: " + str(error) + ".")    
+
+
+
+    #sleep for the configured interval time
+    time.sleep(pollTime)
